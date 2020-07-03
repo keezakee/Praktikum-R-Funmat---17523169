6fdefa9f31aee8f44748fdd7d6590fd7a95d9203
keezakee <41459083+keezakee@users.noreply.github.com>
2020-07-03 20:34:58 +0700

    Add files via upload

---
+++ FM 26 script-17523169.R
@@ -0,0 +1,13 @@
+# Praktikum FM 26
+# Nama  : Muhammad Zaki Alghifari
+# NIM   : 17523169
+
+# Melakukan install package "freqdist"
+install.packages("freqdist")
+
+# Melakukan pengecekan library freqdist
+library("freqdist")
+
+# Mencari Frekuensi dan persentase kumulatif data kolom ke 5
+freqdist(FMA_calonsarjana$K5)
+ 
